f99b2b8f57d126d2ec7d64e754a936d20c8a0933
Ricardo Hatsugai <rhatsugai@gmail.com>
2025-05-29 08:02:13 -0300

    Projeto Spring Boot com as configurações do acesso ao banco de dados acertadas. Arquivos: application.properties e pom.xml

---
+++ vendas-app/src/main/java/br/rep/trindade/vendas_app/GestorVendasApplication.java
@@ -0,0 +1,13 @@
+package br.rep.trindade.vendas_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorVendasApplication.class, args);
+	}
+
+}
---
+++ vendas-app/src/test/java/br/rep/trindade/vendas_app/GestorVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.rep.trindade.vendas_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
